50acb913452a1df3e915a0784d2a26a782418c40
timotewb <timote.wb@gmail.com>
2023-04-22 23:37:49 +1200

    tutorial 4 incomplete

---
+++ react-django-tutorial/music_controller/frontend/src/components/CreateRoomPage.js
@@ -0,0 +1,10 @@
+import React, { Compo, Component } from "react";
+
+export default class CreateRoomPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <p>This is the create room page.</p>;
+  }
+}
---
+++ react-django-tutorial/music_controller/frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,10 @@
+import React, { Compo, Component } from "react";
+
+export default class RoomJoinPage extends Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return <p>This is the room join page.</p>;
+  }
+}
