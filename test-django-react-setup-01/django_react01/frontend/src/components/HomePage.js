e6dc3fd4cd1fcbd097f3b2c2f4726a83848e79ca
timotewb <timote.wb@gmail.com>
2023-04-26 21:23:04 +1200

    test react django not working

---
+++ test-django-react-setup-01/django_react01/frontend/src/components/HomePage.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<p>This is the home page</p>} />
+        </Routes>
+      </Router>
+    );
+  }
+}
